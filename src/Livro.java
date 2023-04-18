6d4aa95916d2000b5f6ebc5ed07dc1bcf70c9c60
EduardoFelipe <felipeeduardogit@gmail.com>
2023-04-18 08:56:07 -0300

    Encapsulamento da classe Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,14 +1,71 @@
 public class Livro {
 
-    String nome;
-    String resumo;
-    double preco;
-    int paginas;
-    String autor;
-    Editora editora;
-    TipoCapaEnum tipoCapa;
+    private String nome;
+    private String resumo;
+    private double preco;
+    private int paginas;
+    private String autor;
+    private Editora editora;
+    private TipoCapaEnum tipoCapa;
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        //if (paginas <= 0)
+            //System.out.println("Paginas Inválidas");
+        //else
+            this.paginas = paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public TipoCapaEnum getTipoCapa() {
+        return tipoCapa;
+    }
+
+    public void setTipoCapa(TipoCapaEnum tipoCapa) {
+        this.tipoCapa = tipoCapa;
+    }
 
     public Livro(String nome, double preco) {
         this.nome = nome; //this é a referencia aos atributos
@@ -31,7 +88,10 @@ public class Livro {
     }
 
     void aplicarDesconto(double percentual) {
-        preco = preco - (preco * percentual);
+        if (percentual > 0.3)
+            System.out.println("Desconto não permitido");
+        else
+            preco = preco - (preco * percentual);
     }
     boolean temEditora(){
         if (editora == null)
--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -3,31 +3,31 @@ public class LivroTeste {
     public static void main(String[] args) {
 
         Livro meuLivro = new Livro("O Hobbit", 9.99);
-        meuLivro.nome = "O Hobbit";
-        meuLivro.paginas = 521;
-        meuLivro.resumo = "bla bla bla bla bla";
-        meuLivro.autor = "J. R. Tolkien";
+        //meuLivro.nome = "O Hobbit";
+        meuLivro.setPaginas(521);
+        meuLivro.setResumo("bla bla bla bla bla");
+        meuLivro.setAutor("J. R. Tolkien");
 
         Editora editora = new Editora();
         //editora.nome = "Felipe";
         editora.email = "felipeeduardo.git@gmail.com";
-        meuLivro.editora = editora;
-        meuLivro.aplicarDesconto(0.1);
+        meuLivro.setEditora (editora);
+        meuLivro.aplicarDesconto(0.5);
 
         meuLivro.exibirDados();
 
         Livro livroFavorito = new Livro("O Código Da Vinci", 27.90);
-        livroFavorito.nome = "O Código Da Vinci";
-        livroFavorito.resumo = "bla bla bla bla bla";
-        livroFavorito.paginas = 500;
-        livroFavorito.autor = "Dan Brown";
+        livroFavorito.setNome ("O Código Da Vinci");
+        livroFavorito.setResumo ("bla bla bla bla bla");
+        livroFavorito.setPaginas (500);
+        livroFavorito.setAutor ("Dan Brown");
         Editora editora2 = new Editora("Felipe");
         editora.nome = "Editora Felipe";
         editora.email = "felipeeduardogit@gmail.com";
 
 
-        livroFavorito.editora = editora2;
-        livroFavorito.tipoCapa = TipoCapaEnum.PERSONALIZADA;
+        livroFavorito.setEditora (editora2);
+        livroFavorito.setTipoCapa (TipoCapaEnum.PERSONALIZADA);
 
         livroFavorito.aplicarDesconto(0.25);
 
